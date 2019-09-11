f1ab33e8a8cf01e5dc703333cac72fb3208ff872
JoelGithub245 <54715459+JoelGithub245@users.noreply.github.com>
2019-09-11 11:10:39 -0400

    Add files via upload

---
+++ Funcion.js
@@ -0,0 +1,10 @@
+
+function login(){
+    var abelardo = document.getElementaryById("user").value;
+var pass = document.getElementaryById("pass").value;
+if(user == "123" && pass == "123")
+alert("funciona") }
+
+
+    
+
